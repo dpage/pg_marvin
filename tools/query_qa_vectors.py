39985394c35033c91b1273c4b131cb1fe4a7b429
Dave Page <dpage@pgadmin.org>
2023-10-06 15:45:39 +0100

    Generic script for asking questions.

--- tools/query_qa_vectors.py
+++ tools/query_qa_vectors.py
@@ -1,63 +1,66 @@
+import argparse
 import textwrap
-import sys
 
 import psycopg
 import torch
 from pgvector.psycopg import register_vector
 from sentence_transformers import SentenceTransformer
-from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import BartTokenizer, BartForConditionalGeneration, pipeline
 
 
-question = sys.argv[1]
+def read_command_line():
+    parser = argparse.ArgumentParser(
+        description='Ask a question to be answered from vectors of text stored in PostgreSQL.')
 
-#
-# Connect to the database, and create our objects
-#
-conn = psycopg.connect("dbname=pg_marvin user=postgres")
-register_vector(conn)
+    parser.add_argument('store', help='the name of the vector store to use. '
+                                      'Equates to a table in PostgreSQL')
 
-#
-# We use the same model we used to generate our embeddings
-# originally to create the query to search the database.
-#
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    parser.add_argument('question', help='the question to be answered')
 
-retriever = SentenceTransformer("flax-sentence-embeddings/all_datasets_v3_mpnet-base", device=device)
+    parser.add_argument('-m', '--max', default=100, help='maximum length of the output in tokens (default: 100)')
 
-#
-# We'll use the BART tokenizer and generator to write the
-# answer text from us, given the context retrieved from the
-# database.
-#
-tokenizer = BartTokenizer.from_pretrained('vblagoje/bart_lfqa')
-generator = BartForConditionalGeneration.from_pretrained('vblagoje/bart_lfqa').to(device)
+    parser.add_argument('-s', '--summary', action='store_true', help='include summarised output')
+
+    parser.add_argument('-t', '--top_k', default=5, help='number of top results to consider')
+
+    parser.add_argument('-o', '--host', default='127.0.0.1',
+                        help='the hostname or IP address of the PostgreSQL server (default: 127.0.0.1)')
+    parser.add_argument('-p', '--port', default='5432',
+                        help='the port number for the PostgreSQL server (default: 5432)')
+    parser.add_argument('-d', '--db', default='postgres', help='the name of the database to use (default: postgres)')
+    parser.add_argument('-u', '--user', default='postgres', help='the name of the database to use (default: postgres)')
+
+    args = parser.parse_args()
+
+    return args
 
 
-def run_query(query, top_k):
+def run_query(query, store, top_k):
     # Encode the query, and use it to find the closest matches in the database
     xq = retriever.encode([query]).tolist()
-    res = conn.execute('SELECT * FROM wiki ORDER BY embedding <-> %s::vector LIMIT %s', (xq[0], top_k)).fetchall()
+    res = conn.execute('SELECT passage_text FROM {} ORDER BY embedding <-> %s::vector LIMIT %s'.format(store),
+                       (xq[0], top_k)).fetchall()
     return res
 
 
 def format_query(query, context):
     # Add the <P> tag to all passages and then join them, then return the
     # question and passages for BART to generate an answer.
-    ctx = [f"<P> {r[1]}" for r in context]
+    ctx = [f"<P> {r[0]}" for r in context]
     ctx = " ".join(ctx)
 
     query = f"question: {query} context: {ctx}"
     return query
 
 
-def generate_answer(query):
+def generate_answer(query, max_length):
     # Tokenize the query to get the input IDs
     inputs = tokenizer([query], return_tensors="pt").to(device)
 
     # Use the generator to predict output IDs
     ids = generator.generate(inputs["input_ids"], num_beams=4,
                              length_penalty=2.0,
-                             max_length=100,
+                             max_length=max_length,
                              min_length=5,
                              no_repeat_ngram_size=3)
 
@@ -67,16 +70,50 @@ def generate_answer(query):
     return answer
 
 
-context = run_query(question, top_k=5)
-result = format_query(question, context)
+if __name__ == '__main__':
+    args = read_command_line()
 
-print('== Result =====================================================================')
-print('Question: \n{}\n'.format(textwrap.fill(question, width=78, initial_indent='    ', subsequent_indent='    ')))
-print('Answer: \n{}'.format(
-    textwrap.fill(generate_answer(result), width=78, initial_indent='    ', subsequent_indent='    ')))
-print('===============================================================================')
+    #
+    # Connect to the database, and create our objects
+    #
+    conn = psycopg.connect('host={} port={} dbname={} user={}'.format(args.host, args.port, args.db, args.user))
+    register_vector(conn)
 
-#
-# Cleanup
-#
-conn.close()
+    #
+    # We use the same model we used to generate our embeddings
+    # originally to create the query to search the database.
+    #
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    retriever = SentenceTransformer("flax-sentence-embeddings/all_datasets_v3_mpnet-base", device=device)
+
+    #
+    # We'll use the BART tokenizer and generator to write the
+    # answer text from us, given the context retrieved from the
+    # database.
+    #
+    tokenizer = BartTokenizer.from_pretrained('vblagoje/bart_lfqa')
+    generator = BartForConditionalGeneration.from_pretrained('vblagoje/bart_lfqa').to(device)
+
+    context = run_query(args.question, args.store, top_k=args.top_k)
+    result = format_query(args.question, context)
+    answer = generate_answer(result, args.max)
+
+    if args.summary:
+        summarizer = pipeline('summarization')
+        summary = summarizer(answer)[0]['summary_text']
+
+    print('== Result =====================================================================')
+    print('Question: \n{}'.format(
+        textwrap.fill(args.question, width=78, initial_indent='    ', subsequent_indent='    ')))
+    print('\nAnswer: \n{}'.format(
+        textwrap.fill(answer, width=78, initial_indent='    ', subsequent_indent='    ')))
+    if args.summary:
+        print('\nSummary: \n{}'.format(
+            textwrap.fill(summary, width=78, initial_indent='    ', subsequent_indent='    ')))
+    print('===============================================================================')
+
+    #
+    # Cleanup
+    #
+    conn.close()
